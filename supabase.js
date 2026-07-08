d2e6d4509c04b8bc91ea93c6dc111c678189f398
hospital976 <hospitalnpr@gmail.com>
2026-07-08 15:14:54 +0530

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,12 @@
+// =============================
+// CAREPLUS SUPABASE CONFIG
+// =============================
+
+const SUPABASE_URL = "https://gaymixxpsnllapithkab.supabase.co";
+
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdheW1peHhwc25sbGFwaXRoa2FiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM0Mzg1NzMsImV4cCI6MjA5OTAxNDU3M30._c9AQ6Wj7IQGNKTp_C3UP8AmlrkSOzfQgwIK5KSwC0E";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
