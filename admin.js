41618699d210dcb8c7ed2f547c30ef684e92f79c
hospital976 <hospitalnpr@gmail.com>
2026-07-08 14:51:28 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -6,7 +6,7 @@
 
 const supabase = window.supabase.createClient(
     "https://YOUR_PROJECT.supabase.co",
-    "YOUR_ANON_KEY"
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdheW1peHhwc25sbGFwaXRoa2FiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM0Mzg1NzMsImV4cCI6MjA5OTAxNDU3M30._c9AQ6Wj7IQGNKTp_C3UP8AmlrkSOzfQgwIK5KSwC0E"
 );
 
 // HTML Elements
