4c550e4f34279902bfbd7ec3fe1745d7cdd4a245
hospital976 <hospitalnpr@gmail.com>
2026-07-08 16:41:50 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,3 +1,4 @@
+let appointments = [];
 // ==============================
 // CAREPLUS ADMIN DASHBOARD
 // ==============================
@@ -152,3 +153,96 @@ function logout() {
 loadAppointments();
 
 setInterval(loadAppointments, 5000);
+
+document.getElementById("searchInput").addEventListener("input", function () {
+
+    const value = this.value.toLowerCase();
+
+    const filtered = appointments.filter(item =>
+
+        item.full_name.toLowerCase().includes(value) ||
+
+        item.phone.toLowerCase().includes(value)
+
+    );
+
+    renderTable(filtered);
+
+});
+
+function renderTable(data){
+
+const table=document.getElementById("appointmentTable");
+
+table.innerHTML="";
+
+if(data.length===0){
+
+table.innerHTML=`
+<tr>
+<td colspan="7">
+No Appointment Found
+</td>
+</tr>
+`;
+
+return;
+
+}
+
+data.forEach(item=>{
+
+table.innerHTML+=`
+<tr>
+
+<td>${item.full_name}</td>
+
+<td>${item.phone}</td>
+
+<td>${item.doctor}</td>
+
+<td>${item.appointment_date}</td>
+
+<td>${item.appointment_time}</td>
+
+<td>
+
+<span class="status ${item.status.toLowerCase()}">
+
+${item.status}
+
+</span>
+
+</td>
+
+<td>
+
+<button class="action-btn approve-btn"
+onclick="changeStatus(${item.id},'Approved')">
+
+Approve
+
+</button>
+
+<button class="action-btn reject-btn"
+onclick="changeStatus(${item.id},'Rejected')">
+
+Reject
+
+</button>
+
+<button class="action-btn delete-btn"
+onclick="deleteAppointment(${item.id})">
+
+Delete
+
+</button>
+
+</td>
+
+</tr>
+`;
+
+});
+
+}
